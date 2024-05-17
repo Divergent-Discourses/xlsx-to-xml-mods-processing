1d6949baf7fe143059aacc7ad63b13f03c109ea5
JamesE6 <61633758+JamesE6@users.noreply.github.com>
2024-05-17 13:28:04 +0100

    Update flat-xml.py

--- flat-xml.py
+++ flat-xml.py
@@ -2,6 +2,7 @@ import pandas as pd
 import xml.etree.ElementTree as ET
 import xml.dom.minidom
 from tqdm import tqdm
+import os
 
 def clean_data(df):
     df.fillna('', inplace=True)
@@ -22,20 +23,13 @@ def save_xml(data, filename):
     except Exception as e:
         print(f"Failed to save XML to {filename}: {e}")
 
-df = pd.read_excel('newspaper_excel_data.xlsx')
-df = clean_data(df)
-
 def escape_special_chars(val):
     if isinstance(val, str):
         return val.replace("&", "&amp;").replace("<", "&lt;").replace(">", "&gt;").replace('"', "&quot;").replace("'", "&apos;")
     return val
 
-df = df.applymap(escape_special_chars)
-
 def sanitize_xml_element_name(name):
     return name.replace(" ", "_").replace("(", "").replace(")", "")
-
-df.columns = [sanitize_xml_element_name(col) for col in df.columns]
 
 def sanitize_column_name(col_name):
     sanitized = col_name.replace('/', '_').replace(' ', '_')
@@ -43,9 +37,24 @@ def sanitize_column_name(col_name):
         sanitized = '_' + sanitized
     return sanitized
 
-df.columns = [sanitize_column_name(col) for col in df.columns]
+def process_excel(fname):
+    df = pd.read_excel(fname)
+    df = clean_data(df)
+    df = df.applymap(escape_special_chars)
+    df.columns = [sanitize_xml_element_name(col) for col in df.columns]
+    df.columns = [sanitize_column_name(col) for col in df.columns]
+    for index, row in tqdm(df.iterrows()):
+        xml_data = row_to_xml(row)
+        try:
+            filename = f'xml_records/{row["DIVERGE_Code"]}.xml'
+            save_xml(xml_data, filename)
+        except:
+            os.mkdir('xml_records')
+            filename = f'xml_records/{row["DIVERGE_Code"]}.xml'
+            save_xml(xml_data, filename)
 
-for index, row in tqdm(df.iterrows()):
-    xml_data = row_to_xml(row)
-    filename = f'xml_records/{row["DIVERGE_Code"]}.xml'
-    save_xml(xml_data, filename)
+if __name__ == "__main__":
+    fname = input('input filepath: ')
+    process_excel(fname)
+    
+    
