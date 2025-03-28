87b27e7006d652b0f6a2f51064171ff92aa47c71
JamesE6 <61633758+JamesE6@users.noreply.github.com>
2025-03-28 15:20:09 +0000

    Update mods_writer_v2.py

--- xml_ver2/mods_writer_v2.py
+++ xml_ver2/mods_writer_v2.py
@@ -2,7 +2,7 @@ from pymods import Mods
 import os
 import pandas as pd
 sourcefile = os.path.dirname(os.path.dirname('Diverge_Newspaper Metadata for XML v2.xlsx'))
-resource = pd.read_excel('test_xl.xlsx')
+resource = pd.read_excel(sourcefile)
 resource = resource.fillna('').astype(str)
 
 for index, row in resource.iterrows():
